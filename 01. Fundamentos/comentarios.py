a70486e5b6065f1d2345956fa23344de5ad2963a
dcunhamaciel <dcunhamaciel@gmail.com>
2024-10-31 21:17:04 -0300

    Comentários

---
+++ 01. Fundamentos/comentarios.py
@@ -0,0 +1,13 @@
+# Minhas variáveis
+salario = 3450.45
+despesas = 2456.20
+
+"""
+A ideia é calcular o
+quanto vai sobrar no
+final do mês!
+"""
+print(salario - despesas)
+
+# print('Fim')
+print('Fim de verdade') # comentário aqui também vale!
