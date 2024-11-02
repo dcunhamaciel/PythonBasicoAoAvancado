51571732b7026005577490381d23436b31a7d1e1
dcunhamaciel <dcunhamaciel@gmail.com>
2024-11-02 10:16:13 -0300

    Operadores Unários

---
+++ 01. Fundamentos/operadores_unarios.py
@@ -0,0 +1,18 @@
+a = 3
+
+# a++ -> não existe
+a += 1
+print(a)
+
+# a-- -> não existe
+a -= 1
+print(a)
+
+print(++a)
+print(-a)
+print(+a)
+
+print(not 0)
+print(not 1)
+print(not False)
+print(not not True)
