7f64a3c39c01f85600478de93add124f3a3f8911
dcunhamaciel <dcunhamaciel@gmail.com>
2024-11-02 11:24:56 -0300

    Tipos String 1

---
+++ 01. Fundamentos/tipos_string_1.py
@@ -0,0 +1,18 @@
+#print(dir(str))
+
+nome = 'Saulo Pedro'
+print(nome)
+print(nome[0])
+
+print("marca d'água")
+print('marca d\'água')
+
+texto = 'Texto entre apóstrofos pode ter "aspas"'
+print(texto)
+
+doc_1 = """Texto com múltiplas...
+... linhas"""
+print(doc_1)
+
+doc_2 = """Texto com múltiplas...\n... linhas"""
+print(doc_2)
