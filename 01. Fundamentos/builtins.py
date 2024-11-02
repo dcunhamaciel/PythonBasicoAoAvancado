4b5e8a9f8f187d3c75ab3faa06244d70cb880771
dcunhamaciel <dcunhamaciel@gmail.com>
2024-11-02 10:44:23 -0300

    Builtins

---
+++ 01. Fundamentos/builtins.py
@@ -0,0 +1,14 @@
+# type: ignore
+print(type(1))
+print(__builtins__.type('Fala Galera!'))
+__builtins__.print(10 / 3)
+
+a = 1 + 2
+
+#__builtins__.help(__builtins__.dir)
+#print(dir())
+#print(dir(__builtins__))
+
+nome = 'João da Silva'
+print(type(nome))
+print(__builtins__.len(nome))
