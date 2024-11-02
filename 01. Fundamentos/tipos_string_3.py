ea7b80bcf0c88677dfebbee1c5a74561cff9126b
dcunhamaciel <dcunhamaciel@gmail.com>
2024-11-02 11:35:20 -0300

    Tipos String 3

---
+++ 01. Fundamentos/tipos_string_3.py
@@ -0,0 +1,10 @@
+frase = 'Python é uma linguagem excelente'
+
+print('py' in frase)
+print('Py' in frase)
+print('ing' not in frase)
+
+print(len(frase))
+print(frase.lower())
+print(frase.upper())
+print(frase.split())
