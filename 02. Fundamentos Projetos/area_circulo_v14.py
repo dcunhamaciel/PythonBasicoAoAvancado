f0ce6031d82a6d6eaef0a90c2aa9b693257d1fa6
dcunhamaciel <dcunhamaciel@gmail.com>
2024-11-03 11:05:48 -0300

    Cálculo Circunferência V14 - Saindo com Erro

---
+++ 02. Fundamentos Projetos/area_circulo_v14.py
@@ -0,0 +1,20 @@
+import sys
+import errno
+from math import pi
+
+def help():
+    print('É necessário informar o raio do círculo.')
+    print('Sintaxe: {} <raio>'.format(sys.argv[0]))
+
+def circulo(raio):
+    return pi * float(raio) ** 2
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        help()
+        sys.exit(errno.EPERM)
+    else:
+        raio = float(sys.argv[1])
+        circunferencia = circulo(raio)
+
+        print('Circunferência: {0}'.format(circunferencia))
