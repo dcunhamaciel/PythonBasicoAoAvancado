1a09f12ed3b187830b9ffb990cb3f1e45659f5c8
dcunhamaciel <dcunhamaciel@gmail.com>
2024-11-03 10:34:03 -0300

    Cálculo Circunferência V7 - Testando Módulo Principal

---
+++ 02. Fundamentos Projetos/area_circulo_v6.py
@@ -0,0 +1,8 @@
+from math import pi
+
+raio = input('Informe o raio: ')
+circunferencia = pi * float(raio) ** 2
+
+print('Circunferência: {0}'.format(circunferencia))
+
+print('Nome do módulo', __name__)
---
+++ 02. Fundamentos Projetos/area_circulo_v7.py
@@ -0,0 +1,7 @@
+from math import pi
+
+if __name__ == '__main__':
+    raio = input('Informe o raio: ')
+    circunferencia = pi * float(raio) ** 2
+
+    print('Circunferência: {0}'.format(circunferencia))
