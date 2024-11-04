1de3fa344f2fcb416157c235698d161e8fb5516d
dcunhamaciel <dcunhamaciel@gmail.com>
2024-11-04 20:45:08 -0300

    For 1

---
+++ 03. Estruturas de Controle/for_1.py
@@ -0,0 +1,9 @@
+for i in range(1, 11):
+    print('i = {}'.format(i))
+
+for i in range(10):
+    print('i = {}'.format(i))
+
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f'{i} * {j} = {i * j}')
