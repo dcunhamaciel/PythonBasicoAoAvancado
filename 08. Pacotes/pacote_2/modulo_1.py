1afedbd7e6d4e9d0469250ee18fa49ad4f7a6b64
dcunhamaciel <dcunhamaciel@gmail.com>
2024-11-17 09:30:26 -0300

    Módulos com Mesmo Nome

---
+++ 08. Pacotes/pacote_2/modulo_1.py
@@ -0,0 +1,4 @@
+print(f'importado módulo "{__name__}" do pacote "{__package__}"!')
+
+def subtracao(x, y):
+    return x - y
---
+++ 08. Pacotes/pacote_v3.py
@@ -0,0 +1,5 @@
+from pacote_1 import modulo_1
+from pacote_2 import modulo_1 as modulo_1_sub
+
+print('Soma', modulo_1.soma(3, 2))
+print('Subtração', modulo_1_sub.subtracao(3, 2))
