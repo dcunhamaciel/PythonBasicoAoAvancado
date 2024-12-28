bfe0cd4ff2f68acfe2dcbd148ba80dcaf11373c5
dcunhamaciel <dcunhamaciel@gmail.com>
2024-12-28 11:31:35 -0300

    Implementando Map #01

---
+++ 11. Programação Funcional/implementando_map_v1.py
@@ -0,0 +1,8 @@
+# Implementação simplificada do map
+
+def mapear(funcao, lista):
+    for elemento in lista:
+        yield funcao(elemento)
+
+if __name__ == '__main__':
+    print(list(mapear(lambda x: x ** 2, [2, 3, 4])))
